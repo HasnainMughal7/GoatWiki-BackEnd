26eb6dbce3d0e7f9a32885c20975c7f2275c5393
GoatWikiAdmin <webeepros@gmail.com>
2025-03-04 14:25:07 +0500

    Update sitemap-generator.js

--- sitemap-generator.js
+++ sitemap-generator.js
@@ -1,10 +1,9 @@
 import ftp from 'basic-ftp';
 import fs from 'fs';
 import { getAllForNavAndAP } from './database.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-
-dotenv.config()
+dotenv.config();
 
 export async function generateSitemap() {
   const baseUrl = 'https://goatwiki.com';
@@ -23,27 +22,37 @@ export async function generateSitemap() {
   ];
 
   try {
-    const dynamicRoutes = PostsArray.map(post => `/Post/${post.metaPermalink}`);
+    // Prepare dynamic routes with lastmod
+    const dynamicRoutes = PostsArray.map(post => {
+      const lastModified = new Date(post.PublishingDate).toISOString().split('T')[0]; // Format: YYYY-MM-DD
+      return `
+        <url>
+          <loc>${baseUrl}/Post/${post.metaPermalink}</loc>
+          <lastmod>${lastModified}</lastmod>
+        </url>`;
+    });
 
-    const allRoutes = [...staticRoutes, ...dynamicRoutes];
+    // Prepare static routes without lastmod
+    const staticRouteUrls = staticRoutes.map(route => `
+      <url>
+        <loc>${baseUrl.replace(/\/$/, '')}/${route.replace(/^\//, '')}</loc>
+      </url>`);
 
     const xmlHeader = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
     const xmlFooter = `</urlset>`;
 
-    const urls = allRoutes.map(route => {
-      return `<url>\n  <loc>${baseUrl.replace(/\/$/, '')}/${route.replace(/^\//, '')}</loc>\n</url>`;
-    }).join('\n');
+    // Combine static and dynamic routes
+    const sitemapContent = xmlHeader + [...staticRouteUrls, ...dynamicRoutes].join('\n') + xmlFooter;
 
-    const sitemapContent = xmlHeader + urls + '\n' + xmlFooter;
-
+    // Save the sitemap file
     fs.writeFileSync('sitemap.xml', sitemapContent);
 
-    // Now upload to Hostinger
+    // Upload to Hostinger
     const response = await uploadToHostinger();
-    return response
+    return response;
   } catch (err) {
     console.error(err);
-    return false
+    return false;
   }
 }
 
@@ -58,14 +67,14 @@ async function uploadToHostinger() {
       user: process.env.FTP_USER,
       password: process.env.FTP_PASS,
       port: 21,
-      secure: false
-    })
+      secure: false,
+    });
 
-    await client.uploadFrom('sitemap.xml', '/public_html/sitemap.xml')
-    return true
+    await client.uploadFrom('sitemap.xml', '/public_html/sitemap.xml');
+    return true;
   } catch (err) {
     console.error('❌ FTP Upload Error:', err);
-    return false
+    return false;
   } finally {
     client.close();
   }
